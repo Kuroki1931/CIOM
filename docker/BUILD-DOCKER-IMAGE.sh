5fac1da6d42ece52cd9ccfd2ed98b66c6de8113a
Kuroki1931 <so.kuroki1931@gmail.com>
2022-11-30 23:14:24 +0900

    modify dockerfile

--- docker/BUILD-DOCKER-IMAGE.sh
+++ docker/BUILD-DOCKER-IMAGE.sh
@@ -6,6 +6,5 @@ BASE_IMAGE=nvidia/cudagl:11.3.0-devel-ubuntu20.04
 DOCKERFILE_NAME=Dockerfile
 
 dpkg -s nvidia-container-runtime > /dev/null 2>&1
-BASE_IMAGE=ubuntu:20.04
 
 docker build . -f ${DOCKERFILE_NAME} -t ${IMAGE_NAME}:${TAG_NAME} --build-arg BASE_IMAGE=${BASE_IMAGE}
